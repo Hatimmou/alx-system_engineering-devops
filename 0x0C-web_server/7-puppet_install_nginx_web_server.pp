df62f1291cec56d4d6225a6bc8c198e73b5283bc
Hatimmou <mounaji97@gmail.com>
2023-10-23 16:54:33 -0700

    install nginx web

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,21 @@
-nstalls a Nginx server
+# automating my work using Puppet
 
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Tolulope05 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+package { 'nginx' :
+  ensure => installed,
+}
+
+file_line { 'install' :
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/Hatimmou permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello world!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
