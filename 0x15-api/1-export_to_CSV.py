3b98efe1d4154b7034ef58c259a80cc6a4062b95
Hatimmou <mounaji97@gmail.com>
2023-10-23 17:45:08 -0700

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,14 +5,27 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    u_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(u_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": u_id}).json()
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
 
-    with open("{}.csv".format(u_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [u_id, username, t.get("completed"), t.get("title")]
-        ) for t in todos]
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user_response = requests.get(url + "users/{}".format(user_id))
+
+    if user_response.status_code != 200:
+        print("User not found. Please provide a valid employee ID.")
+        sys.exit(1)
+
+    user = user_response.json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(["user_id", "username", "completed", "title"])
+
+        for todo in todos:
+            csv_writer.writerow([user_id, username, todo.get("completed"), todo.get("title")]
+)
+
